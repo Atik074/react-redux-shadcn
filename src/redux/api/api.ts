364632f39687366238c7a834f0677c739a50f159
Atik074 <atiksmats@gmail.com>
2024-03-09 23:33:30 +0600

    data post backend with redux

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -4,27 +4,28 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: 'http://localhost:5000',
   }),
+   tagTypes:['todo'] ,
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => ({
-        url: "/task",
-        method: "GET",
+        url: '/task',
+        method: 'GET',
       }),
+      providesTags:['todo']
     }),
     addTodo: builder.mutation({
-      query:(data)=>
-        { 
+      query:(data)=>{ 
           console.log("base Api data" ,data)
-         
          return {
-          url:"/task" ,
-          method:"POST" ,
-          body: data 
+          url:'/task' ,
+          method:'POST' ,
+          body:data
+         
         }
-       
-      }
+      },
+      invalidatesTags:['todo']
     })
   }),
 });
